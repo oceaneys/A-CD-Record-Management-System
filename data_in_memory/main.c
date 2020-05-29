9fe0e3cb5fabd1c1320b71e595bd16b4cac1d3b4
heavyyuan <heavyyuan@tencent.com>
2020-05-30 02:52:39 +0800

    implementation of find_record_by_title

--- data_in_memory/main.c
+++ data_in_memory/main.c
@@ -8,26 +8,60 @@
 
 #include "list.h"
 
-RecordList record_list={0,NULL};
+RecordList record_list={0,LIST_HEAD_INIT(record_list.list)};
+LIST_HEAD(record_result_list);
+LIST_HEAD(track_result_list);
 
 int add_record(struct Record *);
-int add_track(struct Track *);
-int delete_record(char *title);
-int delete_track(char *title);
-int find_record(char *title);
-int find_track(char *title); 
+int add_track(struct Record *, struct Track *);
+int remove_record(struct Record *record);
+int remove_track(struct Record *, struct Track *);
+int find_record_by_title(char *title);
+int find_track_by_title(char *title); 
 
 int add_record(Record *record)
 {
    record_list.record_cnt += 1; 
-   if(record_list.records == NULL){
-        record_list.records = record;
-        return 0;
-   }
-
-   list_add_tail(&record->list,&record_list.records->list);
+   list_add_tail(&record->list,&record_list.list);
    return 0;
 } 
+
+int remove_record(Record *record)
+{
+    if(record_list.record_cnt)
+        record_list.record_cnt -= 1;
+
+    list_del(&record->list);
+    return 0;
+}
+
+
+int add_track(Record *record, Track *track)
+{
+   record->track_count += 1;
+   list_add_tail(&track->list,&record->track);
+   return 0;
+}
+
+int remove_track(Record *record,Track *track)
+{
+    if(record->track_count)
+        record->track_count -= 1;
+    list_del(&track->list);
+    return 0;
+}
+
+int find_record_by_title(char *title)
+{
+    struct list_head *pos,*n = NULL;
+    struct Record *record = NULL;
+    list_for_each_safe(pos, n, &record_list.list){
+        record = container_of(pos, struct Record, list);
+        if(strncmp(title,record->title,sizeof(title)) == 0)
+            list_add(&record->list,&record_result_list);
+    }
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
--- data_in_memory/test.c
+++ data_in_memory/test.c
@@ -9,13 +9,15 @@
 #include "list.h"
 
 RecordList record_list={0,LIST_HEAD_INIT(record_list.list)};
+LIST_HEAD(record_result_list);
+LIST_HEAD(track_result_list);
 
 int add_record(struct Record *);
 int add_track(struct Record *, struct Track *);
-int delete_record(char *title);
-int delete_track(char *title);
-int find_record(char *title);
-int find_track(char *title); 
+int remove_record(struct Record *record);
+int remove_track(struct Record *, struct Track *);
+int find_record_by_title(char *title);
+int find_track_by_title(char *title); 
 
 int add_record(Record *record)
 {
@@ -24,10 +26,41 @@ int add_record(Record *record)
    return 0;
 } 
 
+int remove_record(Record *record)
+{
+    if(record_list.record_cnt)
+        record_list.record_cnt -= 1;
+
+    list_del(&record->list);
+    return 0;
+}
+
+
 int add_track(Record *record, Track *track)
 {
    record->track_count += 1;
    list_add_tail(&track->list,&record->track);
+   return 0;
+}
+
+int remove_track(Record *record,Track *track)
+{
+    if(record->track_count)
+        record->track_count -= 1;
+    list_del(&track->list);
+    return 0;
+}
+
+int find_record_by_title(char *title)
+{
+    struct list_head *pos,*n = NULL;
+    struct Record *record = NULL;
+    list_for_each_safe(pos, n, &record_list.list){
+        record = container_of(pos, struct Record, list);
+        if(strncmp(title,record->title,sizeof(title)) == 0)
+            list_add(&record->list,&record_result_list);
+    }
+    return 0;
 }
 
 int main(int argc, char **argv)
@@ -70,11 +103,19 @@ int main(int argc, char **argv)
         printf("record.artist=%s\n",rtmp->artist);
     }
 
-
+    remove_track(&record1,&track1);
     list_for_each_safe(pos,n,&record1.track){
         Track *rtmp = container_of(pos,struct Track,list);
         printf("record_track=%s\n",rtmp->title);
     }
+
+    printf("\n");
+    find_record_by_title("title1");
+    list_for_each_safe(pos, n, &record_result_list){
+        Record *rtmp = container_of(pos,struct Record,list);
+        printf("record.title=%s\n",rtmp->title);
+    }
+
 
     return 0;
 }
